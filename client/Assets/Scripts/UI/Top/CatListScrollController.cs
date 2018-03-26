41a9991ab1f07eecd5e35418ee5450a8f1040684
Takehiro Iyatomi <iyatomi@gmail.com>
2018-03-26 15:59:31 +0900

    tweak layout

--- client/Assets/Scripts/UI/Top/CatListScrollController.cs
+++ client/Assets/Scripts/UI/Top/CatListScrollController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Numerics;
+
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,12 +10,25 @@ namespace Game.UI {
 public class CatListScrollController : UIBehaviour, IInfiniteScrollSetup {
 
 	public int currentMax_ = 1000; //hack to activate list elements 1st time.
+	public InfiniteScroll infiniteScroll {
+		get { 
+			if (infiniteScroll_ == null) {
+				infiniteScroll_ = GetComponent<InfiniteScroll>();
+			}
+			return infiniteScroll_;
+		}
+	}
+	private InfiniteScroll infiniteScroll_;
 
+	[HideInInspector]
+	public List<KeyValuePair<BigInteger, Neko.Cat>> Cats {
+		get; set;
+	}
+	
 	const int CELL_PER_ELEMENT = 3;
 
 	public void OnPostSetupItems()
 	{
-		var infiniteScroll = GetComponent<InfiniteScroll>();
 		infiniteScroll.onUpdateItem.AddListener(OnUpdateItem);
 		GetComponentInParent<ScrollRect>().movementType = ScrollRect.MovementType.Elastic;
 
@@ -20,14 +36,19 @@ public class CatListScrollController : UIBehaviour, IInfiniteScrollSetup {
 	}
 
 	public void UpdateCurrentMax() {
-		var infiniteScroll = GetComponent<InfiniteScroll>();
-		currentMax_ = ViewModel.ViewModelMgr.instance.Inventory.Cats.Count;
+		currentMax_ = Cats.Count;
 		var limit = (int)((currentMax_ + CELL_PER_ELEMENT - 1) / CELL_PER_ELEMENT);
 
 		var rectTransform = GetComponent<RectTransform>();
 		var delta = rectTransform.sizeDelta;
 		delta.y = infiniteScroll.itemScale * limit;
 		rectTransform.sizeDelta = delta;		
+	}
+
+	public void UpdateCatList(List<KeyValuePair<BigInteger, Neko.Cat>> list) {
+		Cats = list;
+		UpdateCurrentMax();
+		infiniteScroll.RefreshList();
 	}
 
 	public void OnUpdateItem(int itemCount, GameObject obj)
@@ -41,12 +62,12 @@ public class CatListScrollController : UIBehaviour, IInfiniteScrollSetup {
 
 			for (int i = 0; i < CELL_PER_ELEMENT; i++) {
 				var idx = i + itemCount * CELL_PER_ELEMENT;
-				if (idx >= ViewModel.ViewModelMgr.instance.Inventory.Cats.Count) {
+				if (idx >= Cats.Count) {
 					obj.transform.Find("Name" + i).gameObject.SetActive(false);
 					obj.transform.Find("Image" + i).gameObject.SetActive(false);
 					continue;
 				}
-				var kv = ViewModel.ViewModelMgr.instance.Inventory.Cats[idx];
+				var kv = Cats[idx];
 				var name = obj.transform.Find("Name" + i).GetComponent<Text>();
 				var img = obj.transform.Find("Image" + i).GetComponent<Image>();
 				obj.transform.Find("Name" + i).gameObject.SetActive(true);
---
+++ client/Assets/Scripts/UI/Top/TopScreen.cs
@@ -0,0 +1,47 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using uGUI = UnityEngine.UI;
+
+namespace Game.UI {
+class TopScreen : MonoBehaviour {
+    public enum Menu {
+        InventoryMenu,
+        MarketMenu,
+        BreedMenu,
+    }
+    public uGUI.Text balance_;
+    public uGUI.Button[] buttons_;
+    public CatListScrollController scroll_;
+
+    void Start() {
+        balance_ = transform.Find("Balance").gameObject.GetComponent<uGUI.Text>();
+        buttons_[(int)Menu.InventoryMenu].GetComponent<uGUI.Button>().onClick.AddListener(OnInventoryMenu);
+        buttons_[(int)Menu.MarketMenu].GetComponent<uGUI.Button>().onClick.AddListener(OnMarketMenu);
+        buttons_[(int)Menu.BreedMenu].GetComponent<uGUI.Button>().onClick.AddListener(OnBreedMenu);
+        UpdateView();
+        scroll_.Cats = ViewModel.ViewModelMgr.instance.Inventory.Cats;
+    }
+
+    public void UpdateView() {
+        balance_.text = ViewModel.ViewModelMgr.instance.TokenBalance.ToString() + " DBC";
+    }
+
+    void OnInventoryMenu() {
+        Debug.Log("OnInventory");
+        scroll_.UpdateCatList(ViewModel.ViewModelMgr.instance.Inventory.Cats);
+    }
+    void OnMarketMenu() {
+        Debug.Log("Market");
+        //TODO: retrieve list of cats on sale 
+        scroll_.UpdateCatList(new List<KeyValuePair<BigInteger, Neko.Cat>>());        
+    }
+    void OnBreedMenu() {
+        Debug.Log("Breed");        
+        //TODO: retrieve list of cats which can breed 
+        scroll_.UpdateCatList(new List<KeyValuePair<BigInteger, Neko.Cat>>());
+    }
+}
+}
